3c408d4e3546770a970687b08dd9a27263c37228
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-07 11:45:37 +0530

    Add files via upload

---
+++ infinit.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class hallow{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j||i+j==n-1||j==0||j==n-1||i==n/2)
+                System.out.print("*"+" ");
+                else
+                System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+} /*
+*       * 
+* *   * * 
+*   *   * 
+* *   * * 
+*       *   */
