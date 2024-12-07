830f998bc7188036b83bf72d7525a1fd722f4427
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-07 11:47:08 +0530

    Add files via upload

---
+++ tripat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class tripat{
+    public static void main(String args[]){
+        for(int i=0;i<5;i++)
+        {
+            for(int j=i;j<5;j++)
+            {
+                System.out.print("* ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
+
